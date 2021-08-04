e0baf15fdf05ec53ff9b83cbdbada24fd069a7b6
CaptainGazpacho <45720226+CaptainGazpacho@users.noreply.github.com>
2021-08-04 18:42:30 -0500

    I added code to make the bot do stuff

--- ConnorBot.py
+++ ConnorBot.py
@@ -1,2 +1,20 @@
 import discord
+import random
 
+TOKEN = 'ODcxMTY4NTU0MDk4MTMwOTU0.YQXY8A.mneWhS8wxZuHw4IsDH0_kDAMJjM'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('My name is Connor. I’m the android sent by CyberLife to simp for Haruka.'.format(client))
+	
+
+@client.event
+async def on_message(message):
+	username = str(message.author).split('#')[0]
+	user_message = str(message.content)
+	channel = str(message.channel.name)
+	print(f'{username}: {user_message} ({channel})')
+
+client.run(TOKEN)
