d2cdee944ac6a7b7905a5010d291f1bc5b8546ee
CaptainGazpacho <45720226+CaptainGazpacho@users.noreply.github.com>
2021-08-05 12:21:53 -0500

    I MADE THE BOT DO THE THINGS I WANTED IT TO DO!

--- ConnorBot.py
+++ ConnorBot.py
@@ -27,18 +27,36 @@ async def on_message(message):
 async def on_ready():
 	print('My name is Connor. I’m the android sent by CyberLife to simp for Haruka.')
 
-@bot.command(name = 'ConnorHelp', help = 'Describes the purpose of the program')
+@bot.command(name = 'ConnorHelp', help = 'Gives help for all commands')
 async def ConnorHelp(ctx):
-	response = 'This is a program to automatically simp for Haruka'
+	response = 'Here is a list of the available commands:\n\n!ConnorHelp - Gives help for all commands\n!ConnorIntro - Makes the bot introduce itself\n!ConnorSimp - Makes Connor simp for Haruka\n!ConnorGif - Plays a random Connor Gif\n!ConnorQuote - Shows a random Connor quote'
 	await ctx.send(response)
 
 @bot.command(name = 'ConnorIntro', help = 'Makes the bot introduce itself')
-async def connorIntro(ctx):
+async def ConnorIntro(ctx):
 	response = 'My name is Connor. I’m the android sent by CyberLife to simp for Haruka.'
 	await ctx.send(response)
+	
+@bot.command(name = 'ConnorSimp', help = 'Makes Connor simp for Haruka')
+async def ConnorSimp(ctx):
+	response = 'Haruka, I find your antlers to be most, beautiful my lady'
+	await ctx.send(response)
+	
+@bot.command(name = 'ConnorGif', help = 'Plays a random Connor Gif')
+async def ConnorGif(ctx):
+	Connor_Gifs = ['https://tenor.com/view/connor-bryan-dechart-detroit-become-human-dbh-gif-14745350', 'https://tenor.com/view/connor-hank-detroit-become-human-gif-12327020', 'https://tenor.com/view/connor-detroit-detroit-become-human-gif-15593015', 'https://tenor.com/view/wink-connor-detroit-become-human-video-game-gif-17810333', 'https://tenor.com/view/connor-detroit-become-human-dbh-excited-shoulder-wiggle-gif-12020038'
+	]
+	response = random.choice(Connor_Gifs)
+	await ctx.send(response)
+
+@bot.command(name = 'ConnorQuote', help = 'Shows a random Connor quote')
+async def ConnorQuote(ctx):
+	Connor_Quotes = ['28 stab wounds. You didn\'t want to leave him a chance, huh?', 'I like dogs.', 'I\'m not programmed to say things like this, but... I really appreciated working with you. With a little more time, who knows... We might\'ve even become friends...'
+	]
+	response = random.choice(Connor_Quotes)
+	await ctx.send(response)
+	
 	#if message.content == 'HEY CONNOR!':
-		
-		
 
 bot.run(TOKEN)
 
